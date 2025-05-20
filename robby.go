863d05b68610253689de6601f07f01787fddc3b3
k33g <ph.charriere@gmail.com>
2025-05-20 17:13:30 +0000

    💾 Saved.

--- robby.go
+++ robby.go
@@ -5,6 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"os/exec"
+
+	mcp_golang "github.com/metoro-io/mcp-golang"
+	"github.com/metoro-io/mcp-golang/transport/stdio"
 
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
@@ -17,6 +21,9 @@ type Agent struct {
 
 	Tools     []openai.ChatCompletionToolParam
 	ToolCalls []openai.ChatCompletionMessageToolCall
+
+	mcpClient *mcp_golang.Client
+	mcpCmd    *exec.Cmd
 
 	lastError error
 }
@@ -42,6 +49,89 @@ func WithParams(params openai.ChatCompletionNewParams) AgentOption {
 func WithTools(tools []openai.ChatCompletionToolParam) AgentOption {
 	return func(agent *Agent) {
 		agent.Tools = tools
+	}
+}
+
+type STDIOCommandOption []string
+
+func WithDockerMCPToolkit() STDIOCommandOption {
+	return STDIOCommandOption{
+		"docker",
+		"run",
+		"-i",
+		"--rm",
+		"alpine/socat",
+		"STDIO",
+		"TCP:host.docker.internal:8811",
+	}
+}
+func WithSocatMCPToolkit() STDIOCommandOption {
+	return STDIOCommandOption{
+		"socat",
+		"STDIO",
+		"TCP:host.docker.internal:8811",
+	}
+}
+
+func WithMCPClient(command STDIOCommandOption) AgentOption {
+	return func(agent *Agent) {
+
+		cmd := exec.Command(
+			command[0],
+			command[1:]...,
+		)
+
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			agent.lastError = fmt.Errorf("failed to get stdin pipe: %v", err)
+			return
+		}
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			agent.lastError = fmt.Errorf("failed to get stdout pipe: %v", err)
+			return
+		}
+
+		if err := cmd.Start(); err != nil {
+			agent.lastError = fmt.Errorf("failed to start server: %v", err)
+			return
+		}
+
+		clientTransport := stdio.NewStdioServerTransportWithIO(stdout, stdin)
+
+		mcpClient := mcp_golang.NewClient(clientTransport)
+
+		if _, err := mcpClient.Initialize(agent.ctx); err != nil {
+			agent.lastError = fmt.Errorf("failed to initialize client: %v", err)
+			return
+		}
+		agent.mcpClient = mcpClient
+		agent.mcpCmd = cmd
+	}
+}
+
+func WithMCPTools(tools []string) AgentOption {
+	return func(agent *Agent) {
+
+		// Get the tools from the MCP client
+		mcpTools, err := agent.mcpClient.ListTools(agent.ctx, nil)
+		if err != nil {
+			agent.lastError = err
+			return
+		}
+
+		// Convert the tools to OpenAI format
+		filteredTools := []mcp_golang.ToolRetType{}
+		for _, tool := range mcpTools.Tools {
+			for _, t := range tools {
+				if tool.Name == t {
+					filteredTools = append(filteredTools, tool)
+				}
+			}
+		}
+
+		agent.Tools = convertToOpenAITools(filteredTools)
 	}
 }
 
@@ -156,6 +246,42 @@ func (agent *Agent) ExecuteToolCalls(toolsImpl map[string]func(any) (any, error)
 	return responses, nil
 }
 
+func (agent *Agent) ExecuteMCPToolCalls() ([]string, error) {
+
+	responses := []string{}
+	for _, toolCall := range agent.ToolCalls {
+
+		var args map[string]any
+		err := json.Unmarshal([]byte(toolCall.Function.Arguments), &args)
+		if err != nil {
+			return nil, err
+		}
+
+		// Call the tool with the arguments thanks to the MCP client
+		toolResponse, err := agent.mcpClient.CallTool(agent.ctx, toolCall.Function.Name, args)
+		if err != nil {
+			responses = append(responses, fmt.Sprintf("%v", err))
+		} else {
+			if toolResponse != nil && len(toolResponse.Content) > 0 && toolResponse.Content[0].TextContent != nil {
+
+				agent.Params.Messages = append(
+					agent.Params.Messages,
+					openai.ToolMessage(
+						toolResponse.Content[0].TextContent.Text,
+						toolCall.ID,
+					),
+				)
+				responses = append(responses, toolResponse.Content[0].TextContent.Text)
+			}
+		}
+
+	}
+	if len(responses) == 0 {
+		return nil, errors.New("no tool responses found")
+	}
+	return responses, nil
+}
+
 func ToolCallsToJSONString(tools []openai.ChatCompletionMessageToolCall) (string, error) {
 	var jsonData []any
 
@@ -181,4 +307,24 @@ func ToolCallsToJSONString(tools []openai.ChatCompletionMessageToolCall) (string
 		return "", err
 	}
 	return string(jsonString), nil
+}
+
+func convertToOpenAITools(tools []mcp_golang.ToolRetType) []openai.ChatCompletionToolParam {
+	openAITools := make([]openai.ChatCompletionToolParam, len(tools))
+
+	for i, tool := range tools {
+		schema := tool.InputSchema.(map[string]any)
+		openAITools[i] = openai.ChatCompletionToolParam{
+			Function: openai.FunctionDefinitionParam{
+				Name:        tool.Name,
+				Description: openai.String(*tool.Description),
+				Parameters: openai.FunctionParameters{
+					"type":       "object",
+					"properties": schema["properties"],
+					"required":   schema["required"],
+				},
+			},
+		}
+	}
+	return openAITools
 }
